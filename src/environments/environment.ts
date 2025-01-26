3fe13eea1d70322a7a293f63137e64643a2d8609
Naveenkumar <naveenkumaraki1993@gmail.com>
2025-01-26 20:31:40 +0530

    updated code

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCbw3JUXGdV7pfxxhvfhj0R5vJP6FXz2yc",
+    authDomain: "myfirstlearn-f268e.firebaseapp.com",
+    projectId: "myfirstlearn-f268e",
+    storageBucket: "myfirstlearn-f268e.firebasestorage.app",
+    messagingSenderId: "631548592063",
+    appId: "1:631548592063:web:0f6b173869b860cb8f2b0a"
+  }
 };
